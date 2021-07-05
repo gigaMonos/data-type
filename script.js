deaec78bd0adc846173a942fa989f2dd5f20be6f
gigaMonos <yunona.mern@gmail.com>
2021-07-05 20:36:22 +0300

    lesson

---
+++ script.js
@@ -0,0 +1,140 @@
+/*
+let userName; //обьявляем переменную 
+console.log(typeof userName);
+userName = "Freelancer life style";
+console.log(typeof userName);
+userName = 58;
+console.log(typeof userName);
+
+//Undefined - не определенный тип данных 
+let userName;
+console.log(typeof userName);
+
+let userName; //обьявляем переменную
+//получаем тип данных
+console.log(typeof userName);
+//получаем значание пременной
+console.log(userName);
+
+let userName = null;
+console.log(userName);
+
+//пример
+// попытка обратитья к обьекту которого нет
+let block = document.querySelector('block');
+//получаем Null
+console.log(block);
+
+let willYouMarryMe = false;
+if (willYouMarryMe) {
+    console.log(':)')
+} else{
+    console.log(':(')
+}
+
+let userName = 20;
+console.log(typeof userName);
+let userHeight = 1.83
+console.log(typeof userHeight);
+
+//Infinity 
+let getInfinity = 58 / 0;
+console.log(getInfinity);
+console.log(typeof getInfinity);
+
+//-Infinity
+let getInfinity = - 58 / 0;
+console.log(getInfinity);
+console.log(typeof getInfinity);
+
+//Nan
+let getNan = 'Freelancer'/10;
+console.log(getNan)
+console.log(typeof getNan);
+
+// BigInt
+const bigInteger = 12143546576787987967856745645345243523n;
+console.log(typeof bigInteger);
+
+let userName = 'freelancer life style';
+console.log(userName);
+
+let userNameV2 = "Freelancer life style";
+console.log(userNameV2);
+*/
+let userAge = 36;
+let userAgeInfo = `Age: ${userAge}`;
+console.log(userAgeInfo); 
+/*
+//Object
+let userInfo = {
+    name: "freelancer life style",
+    age: 36
+}
+console.log(userInfo);
+console.log(typeof userInfo);
+
+//Symbol
+let id = Symbol("id");
+console.log(typeof id);
+
+//function
+let funcVarible = function name(params){
+
+}
+console.log(funcVarible);
+
+//преобразование типов
+//Строковое преобразование 
+let userAge = 58;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = String(userAge);//функция Стринг 
+//преобразовывает тип Nomber в строчный String
+console.log(userAge);
+console.log(typeof userAge);
+
+let userTrue = true;
+console.log(userTrue);
+console.log(typeof userTrue);
+//пример как Boolean преобразовывается String
+userTrue = String(userTrue);
+console.log(userTrue);
+console.log(typeof userTrue);
+
+// преобразование в Number
+let userAge = "58";
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+console.log(userAge);
+console.log(typeof userAge);
+
+//попытатка преобразовать String v Number
+let userAge = "Freelancer";
+userAge = Number(userAge);
+console.log(userAge);
+console.log(typeof userAge);//получаем Nan
+
+//автоматическое преобразование  
+//без использования функций
+let userAge = "72"/"2";
+console.log(userAge);
+console.log(typeof userAge);
+
+//логическое преобразование
+let userAge = 58;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Boolean(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+//домашка
+let userHeight = 145 / 0;
+console.log(userHeight);
+
